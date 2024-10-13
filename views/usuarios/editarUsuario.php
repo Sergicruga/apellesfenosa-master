e26443cc57e87d32df18e220e452a876ee6f854d
sergicruga <sergio.delacruz@inslapineda.com>
2024-10-13 18:48:43 +0200

    finiquitaoç

--- views/usuarios/editarUsuario.php
+++ views/usuarios/editarUsuario.php
@@ -3,17 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Editar Usuario</title>
-    <link rel="stylesheet" href="styles/usuarios/listado_usuarios.css">
+    <link rel="stylesheet" href="styles/usuarios/editar_usuario.css">
 </head>
 <body>
-<div class="header">
-        <img src="views/obras/Logo2.png" alt="Logo">
+    <div class="header">
+            <img src="images/login/Logo.png" alt="Logo">
     </div>
-    <h2>Editar Usuario</h2>
     <div class="editar">
         <form action="index.php?controller=Usuaris&action=updateUser" method="POST">
             <input type="hidden" name="nombre_original" value="<?= $user['nombre_usuario']; ?>">
-            <input type="text" name="name" value="<?= $user['nombre_usuario']; ?>" required placeholder="Nombre de usuario">
+            <input type="text" name="name" value="<?= $user['nombre_usuario']; ?>" required placeholder="Nombre de usuario">            
             <select id="rol" name="rol">
                 <option value="convidat" <?= $user['rol_usuario'] == 'convidat' ? 'selected' : '' ?>>Convidat</option>
                 <option value="tecnic" <?= $user['rol_usuario'] == 'tecnic' ? 'selected' : '' ?>>Tècnic</option>
@@ -23,10 +22,11 @@
                 <option value="activo" <?= $user['activo'] == 'activo' ? 'selected' : '' ?>>Activo</option>
                 <option value="inactivo" <?= $user['activo'] == 'inactivo' ? 'selected' : '' ?>>Inactivo</option>
             </select>
-
             <button type="submit">Guardar cambios</button>
         </form>
-
+        <div class="image_editar">
+            <img src="images/3.-Busto-de-Dora-Maar.jpg" alt="Statue">
+        </div>
     </div>
 </body>
 </html>
